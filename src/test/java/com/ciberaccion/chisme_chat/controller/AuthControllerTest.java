0a00cdcd07ec98895fc276aead70431a87317bd1
Ciberaccion <argranados@yahoo.com>
2025-12-18 14:07:33 -0600

    corregir falla en test, usar scope test y applicatio-test.properties

--- src/test/java/com/ciberaccion/chisme_chat/controller/AuthControllerTest.java
+++ src/test/java/com/ciberaccion/chisme_chat/controller/AuthControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AuthControllerTest {
     @Autowired UserRepository userRepository;
     @Autowired PasswordEncoder passwordEncoder;
